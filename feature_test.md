2fbe8fd4c99fb9f5c73b4e3ee7c27ca8c0313b09
Steve Kahn <steve@kahn.org.uk>
2019-02-11 14:28:20 +0000

    Balance set to 0 on creation and made test for card top up

---
+++ feature_test.md
@@ -0,0 +1,4 @@
+require "./lib/oystercard.rb"
+
+card = Oystercard.new
+card.top_up(10)
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,4 +5,16 @@ describe Oystercard do
     card = Oystercard.new
     expect(card.balance).to eq(0)
   end
+
+  it "Tops up the card by 10 from 0" do
+    card = Oystercard.new
+    card.top_up(10)
+    expect(card.balance).to eq 10
+  end
+
+  it "Tops up the card by 20 from 0" do
+    card = Oystercard.new
+    card.top_up(20)
+    expect(card.balance).to eq 20
+  end
 end
